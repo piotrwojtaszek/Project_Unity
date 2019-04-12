91c96042bb3334a33d60dcc0d7b42534b87e8fca
piotrwojtaszek <37346684+pietrek15@users.noreply.github.com>
2019-04-12 22:37:59 +0200

    Next

--- Project_Unity_S4/Assets/Scripts/PlayerController.cs
+++ Project_Unity_S4/Assets/Scripts/PlayerController.cs
@@ -11,10 +11,11 @@ public class PlayerController : MonoBehaviour {
     public bool isGrounded;
     public bool facingRight;
 
-    public Transform groundCheck;
+    public Transform groundCheck1;
+    public Transform groundCheck2;
     public float radiuss;
     public LayerMask ground;
-    
+    float speed;
     // Use this for initialization
 
     void OnEnable()
@@ -29,18 +30,40 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         float moveInput = Input.GetAxis("Horizontal");
-        rb2d.velocity = new Vector2 (moveInput * speedForce,rb2d.velocity.y);
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            speed = isGrounded ? speedForce : speedForce * 0.8f;
+            rb2d.velocity = new Vector2(-speed, rb2d.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            speed = isGrounded ? speedForce : speedForce * 0.8f;
+            rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+        }
+        else if (isGrounded)
+        {
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+        }
+        
+        
 
         FlipX(moveInput);
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb2d.AddForce(jumpForce, ForceMode2D.Force);
         }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (rb2d.velocity.y > 0)
+            {
+                rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y * 0.5f);
+            }
 
+        }
 
-
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, radiuss, ground);
+            isGrounded = Physics2D.OverlapArea(groundCheck1.position, groundCheck2.position, ground);
 
 	}
 
@@ -63,10 +86,6 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.white;
-        Gizmos.DrawSphere(groundCheck.position, radiuss);
-    }
+    
 
 }
