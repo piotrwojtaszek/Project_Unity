a6307c205ad653d65687084428bea12dfc79b5db
piotrwojtaszek <37346684+pietrek15@users.noreply.github.com>
2019-04-19 11:50:00 +0200

    NoName
    Improving player movement

--- Project_Unity_S4/Assets/Scripts/PlayerController.cs
+++ Project_Unity_S4/Assets/Scripts/PlayerController.cs
@@ -7,15 +7,26 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     protected Rigidbody2D rb2d;
     public float speedForce = 10f;
+    public float maxSpeed;
     public Vector2 jumpForce;
-    public bool isGrounded;
+    public Vector2 jumpForceWall;
+    public float groundSlide;
+
     public bool facingRight;
 
     public Transform groundCheck1;
     public Transform groundCheck2;
-    public float radiuss;
-    public LayerMask ground;
-    float speed;
+    public LayerMask groundMask;
+    private bool isGrounded;
+
+    public Transform wallCheck;
+    public float wallCheckRadius;
+    private bool isWalled;
+    public LayerMask wallMask;
+    public bool wallSliding;
+
+    
+    
     // Use this for initialization
 
     void OnEnable()
@@ -30,31 +41,32 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         float moveInput = Input.GetAxis("Horizontal");
+        isGrounded = Physics2D.OverlapArea(groundCheck1.position, groundCheck2.position, groundMask);
+        FlipX(rb2d.velocity.x, moveInput);
 
-        if(Input.GetKey(KeyCode.A))
+        rb2d.AddForce(Vector2.right * speedForce * moveInput);
+
+        if(rb2d.velocity.x > maxSpeed)
         {
-            speed = isGrounded ? speedForce : speedForce * 0.8f;
-            rb2d.velocity = new Vector2(-speed, rb2d.velocity.y);
+            rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (rb2d.velocity.x < -maxSpeed)
         {
-            speed = isGrounded ? speedForce : speedForce * 0.8f;
-            rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
-        }
-        else if (isGrounded)
-        {
-            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
         }
         
+
         
 
-        FlipX(moveInput);
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && !wallSliding)
         {
-            rb2d.AddForce(jumpForce, ForceMode2D.Force);
+            if (isGrounded)
+            {
+                rb2d.AddForce(jumpForce, ForceMode2D.Force);
+            }
+            
         }
-        else if (Input.GetButtonUp("Jump"))
+        else if (Input.GetButtonUp("Jump") && !wallSliding)
         {
             if (rb2d.velocity.y > 0)
             {
@@ -63,29 +75,90 @@ public class PlayerController : MonoBehaviour {
 
         }
 
-            isGrounded = Physics2D.OverlapArea(groundCheck1.position, groundCheck2.position, ground);
+    
+        if (!isGrounded)
+        {
+            isWalled = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, wallMask);
+
+            if(facingRight && moveInput > 0.1f || !facingRight && moveInput < -0.1f)
+            {
+                if (isWalled)
+                {
+                    HandleWallSliding();
+                }
+            }
+        }
+
+        if (isGrounded)
+        {
+            
+            GroundMoveTweek(moveInput);
+            
+        }
+
+
+        if(!isWalled || isGrounded)
+        {
+            wallSliding = false;
+        }
+
 
 	}
-
     void FixedUpdate()
     {
-        
+
     }
 
-    void FlipX(float moveInput)
+    void HandleWallSliding()
     {
-        if (moveInput > 0)
+        rb2d.velocity = new Vector2(rb2d.velocity.x, -0.5f);
+
+        wallSliding = true;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (facingRight)
+            {
+                rb2d.AddForce(new Vector2(-1, 3) * jumpForceWall);
+            }
+            else
+            {
+                rb2d.AddForce(new Vector2(1, 3) * jumpForceWall);
+            }
+        }
+
+    }
+
+    
+
+    void FlipX(float vel, float moveInput)
+    {
+        if (vel > 0 && moveInput> 0)
         {
             transform.localScale = new Vector3(1, 1, 1);
             facingRight = true;
         }
-        else if (moveInput < 0)
+        else if (vel < 0 && moveInput < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
             facingRight = false;
         }
     }
 
+    void GroundMoveTweek(float moveInput)
+    {
+        if (moveInput == 0)
+        {
+            if (rb2d.velocity.x > 1f)
+            {
+                rb2d.AddForce(Vector2.right * -groundSlide);
+            }
+            else if (rb2d.velocity.x < -1f)
+            {
+                rb2d.AddForce(Vector2.right * groundSlide);
+            }
+        }
+    }
     
 
 }
